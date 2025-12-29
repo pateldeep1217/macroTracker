fedc00a2f75effb5130aa5c1e74d6930968e09d4
Deep Patel <patel_deep1217@outlook.com>
2025-12-28 21:29:35 -0500

    fix: force dark mode using next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,14 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className="dark dark:text-white dark:bg-zinc-950 antialiased"
-    >
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class" forcedTheme="dark">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
